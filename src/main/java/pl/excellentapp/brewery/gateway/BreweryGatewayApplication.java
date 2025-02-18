1e1c2406702f9a8acfd027c863d9a66fa16006a3
Adrian Pionka <adrian@pionka.com>
2025-02-18 20:19:25 +0100

    init brewery gateway

---
+++ src/main/java/pl/excellentapp/brewery/gateway/BreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package pl.excellentapp.brewery.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/excellentapp/brewery/gateway/BreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.excellentapp.brewery.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
